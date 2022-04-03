2efaa5a2d5c377e8692f260ec45f765d7f943cfb
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-03 22:16:48 +0200

    from 3
    from 3
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
+
 def say_hello(request):
-    return render(request, 'Hello.html')
+ 
+    return render(request, 'Hello.html', { 'name': 'Aabenraa'})
 
 
 
